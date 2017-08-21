f51ddadd99e961ba0db9cc3df087819fc0a73daf
joshlacey <williamjoshualacey@gmail.com>
2017-08-21 16:10:57 -0400

    added project associations

--- app/models/projects.rb
+++ app/models/projects.rb
@@ -1,6 +1,9 @@
 class Project < ApplicationRecord
 has_many :user_projects
 has_many :projects, through: :user_projects
+has_many :boards
+has_many :comments
+has_many :images, through: :boards
 
 
 end
