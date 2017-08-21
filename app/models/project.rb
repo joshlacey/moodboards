328e76e98a4de1f7b9a72b4b15589e6241e5da04
joshlacey <williamjoshualacey@gmail.com>
2017-08-21 16:46:51 -0400

    udated has_many assocation for user in Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
 has_many :user_projects
-has_many :projects, through: :user_projects
+has_many :users, through: :user_projects
 has_many :boards
 has_many :comments
 has_many :images, through: :boards
