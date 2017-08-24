ef34c5fb0128b78f25f8f841602111b16ce53fbb
joshlacey <williamjoshualacey@gmail.com>
2017-08-24 16:27:07 -0400

    changed some things

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_many :user_projects
 	has_many :projects, through: :user_projects
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	has_many :images
 
 	has_secure_password
