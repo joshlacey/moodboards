31843ee3ccfb2e4b7c9854a5c3cb7dd53edd0e09
Tim Freeman <d.timothy.freeman@gmail.com>
2017-08-24 10:37:30 -0400

    added back buttons to image and board shows

--- db/schema.rb
+++ db/schema.rb
@@ -36,10 +36,6 @@ ActiveRecord::Schema.define(version: 20170824030012) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
     t.string "photo_file_name"
     t.string "photo_content_type"
     t.integer "photo_file_size"
