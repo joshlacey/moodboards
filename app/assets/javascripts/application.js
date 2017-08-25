da103d9b0ac7f8694936a7de827a2f430327c1a3
joshlacey <williamjoshualacey@gmail.com>
2017-08-25 13:56:05 -0400

    styling updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,3 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-// Loads all Semantic javascripts
-//= require semantic-ui
-//= require semantic-ui/modal
-//= require semantic-ui/dropdown
