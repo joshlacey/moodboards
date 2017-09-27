2481d38b6d8509ba1488cb02a7c26518b54f2e09
joshlacey <williamjoshualacey@gmail.com>
2017-09-27 17:47:38 -0400

    changed validation down

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,10 +2,10 @@ class Image < ApplicationRecord
 	belongs_to :board
 	belongs_to :user
 	has_many :comments
-	has_attached_file :photo, default_url: "/images/:style/missing.png"
-	validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
-	validates :url, :url => true, :allow_blank => true
-	validates_format_of :url, :with => /\.(jpe?g|png|gif|bmp)\z/i, :on => :create
+	#has_attached_file :photo, default_url: "/images/:style/missing.png"
+	#validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+	#validates :url, :url => true, :allow_blank => true
+	#validates_format_of :url, :with => /\.(jpe?g|png|gif|bmp)\z/i, :on => :create
 
 def file_for
 	if self.url?
