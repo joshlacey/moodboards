f1e5918a5dff51b97dad972ef3af33f9ceb4b84e
joshlacey <williamjoshualacey@gmail.com>
2017-09-27 15:35:07 -0400

    setup pg database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170824030012) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.string "title"
     t.integer "project_id"
