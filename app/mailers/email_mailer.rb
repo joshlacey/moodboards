e38dcefb831d14e24349801803968fb2263b7b53
joshlacey <williamjoshualacey@gmail.com>
2017-09-28 09:18:16 -0400

    changed email address

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -1,39 +1,39 @@
 class EmailMailer < ApplicationMailer
-	
+
 	def email_approval(user, project)
 		@user = user
 		@project = project
-		mail(to: 'd.timothy.freeman@gmail.com', subject: "#{@project.title}, was approved!")
+		mail(to: 'williamjoshualacey@gmail.com.com', subject: "#{@project.title}, was approved!")
 	end
 
 	def email_rejection(user, project)
 		@user = user
 		@project = project
-		mail(to: 'd.timothy.freeman@gmail.com', subject: "#{@project.title}, was rejected.")
+		mail(to: 'williamjoshualacey@gmail.com.com', subject: "#{@project.title}, was rejected.")
 	end
 
 	def email_user(user, subject, body)
 		@user = user
 		@body = body
-		mail(to: 'd.timothy.freeman@gmail.com', subject: subject)
+		mail(to: 'williamjoshualacey@gmail.com.com', subject: subject)
 	end
 
 	def notify_image_comment(user, comment)
 		@user = user
 		@comment = comment
-		mail(to: 'd.timothy.freeman@gmail.com', subject: "#{@comment.user.full_name} just commented on an image in #{@comment.image.board.project.title}!")
+		mail(to: 'williamjoshualacey@gmail.com.com', subject: "#{@comment.user.full_name} just commented on an image in #{@comment.image.board.project.title}!")
 	end
 
 	def notify_board_comment(user, comment)
 		@user = user
 		@comment = comment
-		mail(to: 'd.timothy.freeman@gmail.com', subject: "#{@comment.user.full_name} just commented on the #{@comment.board.title} board in #{@comment.board.project.title}!")
+		mail(to: 'williamjoshualacey@gmail.com.com', subject: "#{@comment.user.full_name} just commented on the #{@comment.board.title} board in #{@comment.board.project.title}!")
 	end
 
 	def notify_project_comment(user, comment)
 		@user = user
 		@comment = comment
-		mail(to: 'd.timothy.freeman@gmail.com', subject: "#{@comment.user.full_name} just commented on your #{@comment.project.title} project!")
+		mail(to: 'williamjoshualacey@gmail.com.com', subject: "#{@comment.user.full_name} just commented on your #{@comment.project.title} project!")
 	end
 
 end
